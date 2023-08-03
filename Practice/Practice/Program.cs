da034dcbcd313385278c03282e918c95aaf997c1
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2023-08-03 20:17:35 +0500

    Create practice two solution

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Practice;
+﻿using System.Text.RegularExpressions;
+
+namespace Practice;
 
 public class Program
 {
+    private static Regex alphabet = new Regex(@"[a-z]");
+
     public static void Main()
     {
+        Console.WriteLine("Input string:");
         var str = Console.ReadLine().Trim();
+
+        var incorrectChars = str.FindOutsideAlphabetChars(alphabet);
+
+        if (incorrectChars.Length != 0)
+        {
+            Console.WriteLine("The string contains invalid characters: " + string.Join(", ", incorrectChars));
+            return;
+        }
+
+        Console.WriteLine("Result:");
         Console.WriteLine(PracticeOneSolution(str));
     }
 
@@ -21,5 +36,21 @@ public class Program
             var strReverse = str.Reverse();
             return string.Join("", strReverse) + str;
         }
+    }
+}
+
+public static class StringExtends
+{
+    private static Regex defaultAlphabet = new Regex(@"[a-zA-Z0-9]\s");
+
+    public static char[] FindOutsideAlphabetChars(this string str, Regex? alphabet = null)
+    {
+        alphabet ??= defaultAlphabet;
+
+        var incorrectChars = alphabet
+            .Replace(str, "")
+            .ToCharArray();
+
+        return incorrectChars;
     }
 }
