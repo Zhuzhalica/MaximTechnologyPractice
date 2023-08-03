93c45d73a5745440c9960e08ea33df06c7ac2f06
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2023-08-03 18:52:25 +0500

    Create practice one solution

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Practice;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main()
+    {
+        var str = Console.ReadLine().Trim();
+        Console.WriteLine(PracticeOneSolution(str));
+    }
+
+    public static string PracticeOneSolution(string str)
+    {
+        if (str.Length % 2 == 0)
+        {
+            var headReverse = str.Substring(0, str.Length / 2).Reverse();
+            var tailReverse = str.Substring(str.Length / 2, str.Length / 2).Reverse();
+            return string.Join("", headReverse) + string.Join("", tailReverse);
+        }
+        else
+        {
+            var strReverse = str.Reverse();
+            return string.Join("", strReverse) + str;
+        }
+    }
+}
