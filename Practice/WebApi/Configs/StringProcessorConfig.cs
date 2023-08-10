f2b75864e769ecbf42a0d32e538a974eb0dfc511
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2023-08-10 12:24:25 +0500

    ParallelLimit start

--- Practice/WebApi/Configs/StringProcessorConfig.cs
+++ Practice/WebApi/Configs/StringProcessorConfig.cs
@@ -4,4 +4,5 @@ public class StringProcessorConfig
 {
     public string RandomApi { get; set; }
     public string[] BlackList { get; set; }
+    public int ParallelLimit { get; set; }
 }
--- Practice/WebApi/Controllers/StringProcessorController.cs
+++ Practice/WebApi/Controllers/StringProcessorController.cs
@@ -10,21 +10,36 @@ public class StringProcessorController : ControllerBase
     private readonly ILogger<StringProcessorController> _logger;
     private readonly StringProcessor _stringProcessor;
 
-    public StringProcessorController(ILogger<StringProcessorController> logger, StringProcessor stringProcessor)
+    public StringProcessorController(ILogger<StringProcessorController> logger, StringProcessor stringProcessor, StringProcessorConfig config)
     {
         _logger = logger;
         _stringProcessor = stringProcessor;
+        _config = config;
     }
+
+    private static int currentCountConnection = 0;
+    private readonly StringProcessorConfig _config;
 
     [HttpGet(Name = "GetWeatherForecast")]
     public ObjectResult GetProcessedString(string inputString, SortType sortType)
     {
+        currentCountConnection++;
+        Console.WriteLine(currentCountConnection);
+        if (currentCountConnection > _config.ParallelLimit)
+        {
+            currentCountConnection--;
+            return StatusCode(503, "");
+        }
+
         try
         {
-            return StatusCode(200, _stringProcessor.Run(inputString, sortType));
+            var result = _stringProcessor.Run(inputString, sortType);
+            currentCountConnection--;
+            return StatusCode(200, result);
         }
         catch (ArgumentException e)
         {
+            currentCountConnection--;
             return StatusCode(400, $"{e.Message}");
         }
     }
