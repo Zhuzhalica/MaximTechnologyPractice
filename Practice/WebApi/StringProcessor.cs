4bd49827d69fc379c287b27e12ff5a07cef62a26
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2023-08-10 14:42:49 +0500

    Comment thread sleep

--- Practice/WebApi/StringProcessor.cs
+++ Practice/WebApi/StringProcessor.cs
@@ -18,7 +18,7 @@ public class StringProcessor
 
     public string Run(string str, SortType sortType)
     {
-        Thread.Sleep(10000);
+        // Thread.Sleep(10000);
         CheckCorrectInputString(str);
         
         var processedString = StringProcessing(str);
